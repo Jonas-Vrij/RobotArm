5865b5df7c098529e4542e53f85f59a0f3ab10b5
Jonas-Vrij <142597084+Jonas-Vrij@users.noreply.github.com>
2023-09-09 12:30:24 +0200

    Update ArmWithGui.py
    changed the axis slider t oa seperate window, with a topdown view

--- Arm/ArmWithGui.py
+++ Arm/ArmWithGui.py
@@ -8,6 +8,7 @@ from Pathplanning import createNodes
 
 endX = 0
 endY = 0
+topAngle = 0
 ports = serial.tools.list_ports.comports()
 serialInst = serial.Serial()
 portsList = []
@@ -18,8 +19,9 @@ inverseAngles = False
 
 
 def returnAngles(xCoord, yCoord, drawLines, inverse):
+    global topAngle
     if yCoord == a + c:
-        return "90_180_0"
+        return f"90_180_0_{facing_slider.get()}"
 
     goalX = xCoord
     goalY = yCoord
@@ -38,7 +40,7 @@ def returnAngles(xCoord, yCoord, drawLines, inverse):
         angle_A = 360 - angle_C
         angle_B = 360 - angle_B
 
-    outStr = f"{round(angle_A)}_{round(angle_B)}_{slider.get()}"
+    outStr = f"{round(angle_A)}_{round(angle_B)}_{topAngle}_{facing_slider.get()}"
     if drawLines:
         updateLines(round(angle_A), round(angle_B))
 
@@ -87,28 +89,38 @@ def line_crosses_square(square_vertices, line_points):
 
 
 def drag_start(event):
-    label.startX = event.x
-    label.startY = event.y
+    if event.widget.name == "side":
+        label.startX = event.x
+        label.startY = event.y
+    elif event.widget.name == "top":
+        topLabel.startX = event.x
+        topLabel.startY = event.y
 
 
 def drag_motion(event):
     global endAngle, inverseAngles, endX, endY
-    pX = label.winfo_x() - label.startX + event.x
-    y = label.winfo_y() - label.startY + event.y
-    updateCoords(pX, y)
+    if event.widget.name == "side":
+        pX = label.winfo_x() - label.startX + event.x
+        pY = label.winfo_y() - label.startY + event.y
+        updateCoords(pX, pY)
+    elif event.widget.name == "top":
+        pX = topLabel.winfo_x() - topLabel.startX + event.x
+        pY = topLabel.winfo_y() - topLabel.startY + event.y
+        updateTopCoords(pX, pY)
 
-def updateCoords(pX, y):
-    global inverseAngles
-    angles = returnAngles(pX, 500 - y, False, inverseAngles).split('_')
+
+def updateCoords(pX, pY):
+    global inverseAngles, topAngle
+    angles = returnAngles(pX, 500 - pY, False, inverseAngles).split('_')
     foreArmPoints = draw_rotated_line(None, None, a, 0, 500, int(angles[0]), False)
     foreArmPoints = [(0, 0), (foreArmPoints[0], 500 - foreArmPoints[1])]
-    armPoints = [(foreArmPoints[1][0], foreArmPoints[1][1]), (pX, 500 - y)]
+    armPoints = [(foreArmPoints[1][0], foreArmPoints[1][1]), (pX, 500 - pY)]
 
-    # check for Minimal value if arms arnt same length
+    # check for Minimal value if arms aren't same length
     if a != c:
-        if (pX ** 2) + ((500 - y) ** 2) < (a - c) ** 2:
+        if (pX ** 2) + ((500 - pY) ** 2) < (a - c) ** 2:
             return 0
-    if (pX ** 2) + ((500 - y) ** 2) > (a + c) ** 2:
+    if (pX ** 2) + ((500 - pY) ** 2) > (a + c) ** 2:
         return 0
 
     # check if arms cross with obstacles
@@ -125,10 +137,10 @@ def updateCoords(pX, y):
             else:
                 inverseAngles = False
     if inverseAngles:
-        angles = returnAngles(pX, 500 - y, False, True).split('_')
+        angles = returnAngles(pX, 500 - pY, False, True).split('_')
         foreArmPoints = draw_rotated_line(None, None, a, 0, 500, int(angles[0]), False)
         foreArmPoints = [(0, 0), (foreArmPoints[0], 500 - foreArmPoints[1])]
-        armPoints = [(foreArmPoints[1][0], foreArmPoints[1][1]), (pX, 500 - y)]
+        armPoints = [(foreArmPoints[1][0], foreArmPoints[1][1]), (pX, 500 - pY)]
         for i in obstructions:
             if line_crosses_square(i, foreArmPoints):
                 print("Point not in Range!")
@@ -136,16 +148,36 @@ def updateCoords(pX, y):
             if line_crosses_square(i, armPoints):
                 print("Point not in Range!")
                 return 0
-    returnAngles(pX, 500 - y, True, inverseAngles)
-    label.place(x=pX - 10, y=y - 10)
+    returnAngles(pX, 500 - pY, True, inverseAngles)
+    label.place(x=pX - 10, y=pY - 10)
     label.lift()
     # Update the coordinates label text
-    endX, endY = [pX, 500 - y]
-    coordinates_label.config(text=f"({pX}, {500 - y})")
-    coordinates_label.place(x=pX - 10, y=y - 30)
+    endX, endY = [pX, 500 - pY]
+    coordinates_label.config(text=f"({pX}, {500 - pY})")
+    coordinates_label.place(x=pX - 10, y=pY - 30)
 
-    angles = returnAngles(pX, 500 - y, True, inverseAngles).split('_')
-    anglesLabel.config(text=f"A:{angles[0]}, B:{angles[1]}, C:{endAngle+facing_slider.get()}")
+    angles = returnAngles(pX, 500 - pY, True, inverseAngles).split('_')
+    anglesLabel.config(text=f"A:{angles[0]}, B:{angles[1]}")
+
+
+def updateTopCoords(pX, pY):
+    global topAngle
+    c = 200 - pX
+    a = pY
+    b = math.sqrt((a ** 2) + (c ** 2))
+
+    num1 = (a ** 2) + (b ** 2) - (c ** 2)
+    num2 = 2 * a * b
+    angle = math.acos(num1 / num2)
+    angle = math.degrees(angle)
+    if pX < 200: drawAngle = angle + 90
+    elif pX > 200: drawAngle = 90 - angle
+    else: drawAngle = -90
+    topAngle = drawAngle
+    draw_rotated_line(topCanvas, topLine, 150, 200, 0, -drawAngle, True)
+    topAngleLabel.config(text=f"C: {int(topAngle)}")
+    topLabel.place(x=pX, y=pY)
+    topLabel.lift()
 
 
 def draw_rotated_line(pCanvas, line_id, length, pX, y, angle, draw):
@@ -189,13 +221,6 @@ def changePos():
     # except:
     print("No Valid Input!")
     pass
-
-
-def loopToZero(num):
-    if num == 360:
-        slider.set(0)
-    elif num == -1:
-        slider.set(359)
 
 
 endAngle = 0
@@ -324,6 +349,7 @@ canvas.place(relx=0, rely=0)
 # Create the draggable label
 label = Label(window, bg="blue", width=2, height=1)
 label.place(x=0, y=500)
+label.name = "side"
 
 reset = Button(window, text="Reset", command=resetPos)
 reset.place(x=0, y=40)
@@ -332,11 +358,6 @@ entry = Entry(window)
 entry.place(x=0, y=60)
 change = Button(window, text="Change Position", command=changePos)
 change.place(x=0, y=80)
-
-# rotation axis slider
-slider = Scale(window, length=300, from_=-1, to=360, orient="horizontal", command=loopToZero)
-slider.config(command=lambda num: (loopToZero(int(num))))
-slider.place(x=500, y=0)
 
 # facing direction slider
 facing_slider = Scale(window, length=300, from_=90, to=-90, orient="vertical", command=updateFacing)
@@ -358,4 +379,20 @@ label.lift()
 label.bind("<Button-1>", drag_start)
 label.bind("<B1-Motion>", drag_motion)
 canvas.bind("<Button-1>", canvas_click)
+
+topDown_window = Toplevel(window)
+topDown_window.geometry("400x300")
+# create topdown window canvas
+topCanvas = Canvas(topDown_window, width=5000, height=5000)
+topCanvas.pack()
+topCanvas.create_oval(200 - 150, -150, 200 + 150, 150, outline="gray", width=2)
+topLine = topCanvas.create_line(0, 500, 20, 500, fill='green', width=2)
+# create movable Lable
+topLabel = Label(topDown_window, bg="blue", width=2, height=1)
+topLabel.place(x=200, y=150)
+topLabel.name = "top"
+topAngleLabel = Label(topDown_window, text="C:0")
+topAngleLabel.place(x=0, y=20)
+topLabel.bind("<Button-1>", drag_start)
+topLabel.bind("<B1-Motion>", drag_motion)
 window.mainloop()
